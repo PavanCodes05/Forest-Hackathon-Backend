32b583a16a4e57f022d2d373afa1c43c531508c5
PavanCodes05 <pavancodes05@gmail.com>
2025-03-14 13:20:25 +0530

    Live Notifcation Demo

--- app/api/routes/devices.py
+++ app/api/routes/devices.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException
 from app.schemas.device import Device
-from app.firebase_client import db
+from app.firebase_client import db, messaging
 from datetime import datetime
 
 router = APIRouter()
@@ -14,7 +14,19 @@ async def add_device(device: Device):
             device.installation_date = datetime.today().strftime("%Y-%m-%d")
         new_device_ref = device_collection.push(device.model_dump(exclude={"id"}))
         device.id = new_device_ref.key  # Firebase returns the generated key
+
+        message = messaging.Message(
+            notification=messaging.Notification(
+                title="New Device Added",
+                body="Notification Body Test"
+            ),
+            data={"type": "Gunshot", "confidence": "100%"},
+            topic="all"
+        )
+        messaging.send(message)
+
         return device
+    
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error adding device: {str(e)}")
 
--- app/firebase_client.py
+++ app/firebase_client.py
@@ -1,6 +1,6 @@
 import os
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, db, messaging
 
 cred = credentials.Certificate("firebase_cred.json")
 firebase_admin.initialize_app(cred, {
@@ -8,3 +8,4 @@ firebase_admin.initialize_app(cred, {
 })
 
 db = db.reference()
+messaging = messaging
